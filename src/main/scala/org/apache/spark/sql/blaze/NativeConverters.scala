3c66822b3229c181e5cd302ba056e540270b61e5
zhangli20 <zhangli20@kuaishou.com>
2022-01-25 18:23:20 +0800

    Supports more expression convertions

--- src/main/scala/org/apache/spark/sql/blaze/NativeConverters.scala
+++ src/main/scala/org/apache/spark/sql/blaze/NativeConverters.scala
@@ -1,11 +1,26 @@
 package org.apache.spark.sql.blaze
 
+import scala.collection.JavaConverters._
+import scala.reflect.ClassTag.Nothing
+
+import com.univocity.parsers.annotations.Trim
+import org.apache.spark.sql.catalyst.expressions.Abs
+import org.apache.spark.sql.catalyst.expressions.Acos
 import org.apache.spark.sql.catalyst.expressions.Add
 import org.apache.spark.sql.catalyst.expressions.And
+import org.apache.spark.sql.catalyst.expressions.Asin
+import org.apache.spark.sql.catalyst.expressions.Atan
 import org.apache.spark.sql.catalyst.expressions.AttributeReference
+import org.apache.spark.sql.catalyst.expressions.Cast
+import org.apache.spark.sql.catalyst.expressions.Ceil
+import org.apache.spark.sql.catalyst.expressions.Concat
+import org.apache.spark.sql.catalyst.expressions.Cos
+import org.apache.spark.sql.catalyst.expressions.DatePart
 import org.apache.spark.sql.catalyst.expressions.Divide
 import org.apache.spark.sql.catalyst.expressions.EqualTo
+import org.apache.spark.sql.catalyst.expressions.Exp
 import org.apache.spark.sql.catalyst.expressions.Expression
+import org.apache.spark.sql.catalyst.expressions.Floor
 import org.apache.spark.sql.catalyst.expressions.GreaterThan
 import org.apache.spark.sql.catalyst.expressions.GreaterThanOrEqual
 import org.apache.spark.sql.catalyst.expressions.IsNotNull
@@ -13,10 +28,28 @@ import org.apache.spark.sql.catalyst.expressions.LessThan
 import org.apache.spark.sql.catalyst.expressions.LessThanOrEqual
 import org.apache.spark.sql.catalyst.expressions.Like
 import org.apache.spark.sql.catalyst.expressions.Literal
+import org.apache.spark.sql.catalyst.expressions.Log
+import org.apache.spark.sql.catalyst.expressions.Log10
+import org.apache.spark.sql.catalyst.expressions.Log2
+import org.apache.spark.sql.catalyst.expressions.Lower
+import org.apache.spark.sql.catalyst.expressions.Md5
 import org.apache.spark.sql.catalyst.expressions.Multiply
 import org.apache.spark.sql.catalyst.expressions.Not
+import org.apache.spark.sql.catalyst.expressions.NullIf
+import org.apache.spark.sql.catalyst.expressions.OctetLength
 import org.apache.spark.sql.catalyst.expressions.Or
+import org.apache.spark.sql.catalyst.expressions.Round
+import org.apache.spark.sql.catalyst.expressions.Sha2
+import org.apache.spark.sql.catalyst.expressions.Signum
+import org.apache.spark.sql.catalyst.expressions.Sin
+import org.apache.spark.sql.catalyst.expressions.Sqrt
+import org.apache.spark.sql.catalyst.expressions.StringTrim
+import org.apache.spark.sql.catalyst.expressions.StringTrimLeft
+import org.apache.spark.sql.catalyst.expressions.StringTrimRight
 import org.apache.spark.sql.catalyst.expressions.Subtract
+import org.apache.spark.sql.catalyst.expressions.Tan
+import org.apache.spark.sql.catalyst.expressions.TruncDate
+import org.apache.spark.sql.catalyst.expressions.Upper
 import org.apache.spark.sql.types.BinaryType
 import org.apache.spark.sql.types.BooleanType
 import org.apache.spark.sql.types.ByteType
@@ -35,10 +68,13 @@ import org.ballistacompute.protobuf.ArrowType
 import org.ballistacompute.protobuf.EmptyMessage
 import org.ballistacompute.protobuf.Field
 import org.ballistacompute.protobuf.PhysicalBinaryExprNode
+import org.ballistacompute.protobuf.PhysicalCastNode
 import org.ballistacompute.protobuf.PhysicalColumn
 import org.ballistacompute.protobuf.PhysicalExprNode
 import org.ballistacompute.protobuf.PhysicalIsNotNull
 import org.ballistacompute.protobuf.PhysicalNot
+import org.ballistacompute.protobuf.PhysicalScalarFunctionNode
+import org.ballistacompute.protobuf.ScalarFunction
 import org.ballistacompute.protobuf.ScalarValue
 import org.ballistacompute.protobuf.Schema
 import org.ballistacompute.protobuf.Timestamp
@@ -103,36 +139,102 @@ object NativeConverters {
   }
 
   def convertExpr(sparkExpr: Expression): PhysicalExprNode = {
-    def buildExprNode(buildFn: (PhysicalExprNode.Builder) => Unit): PhysicalExprNode = {
-      val builder = PhysicalExprNode.newBuilder()
-      buildFn(builder)
-      builder.build()
+    def buildExprNode(buildFn: (PhysicalExprNode.Builder) => PhysicalExprNode.Builder): PhysicalExprNode =
+      buildFn(PhysicalExprNode.newBuilder()).build()
+
+    def buildBinaryExprNode(left: Expression, right: Expression, op: String): PhysicalExprNode = buildExprNode {
+      _.setBinaryExpr(PhysicalBinaryExprNode.newBuilder()
+        .setL(convertExpr(left))
+        .setR(convertExpr(right))
+        .setOp(op)
+        .build())
     }
-    def buildBinaryExprNode(lhs: PhysicalExprNode, rhs: PhysicalExprNode, op: String): PhysicalExprNode = {
-      buildExprNode(_.setBinaryExpr(PhysicalBinaryExprNode.newBuilder().setL(lhs).setR(rhs).setOp(op).build()))
+
+    def buildScalarFunction(fn: ScalarFunction, args: Seq[Expression], dataType: DataType): PhysicalExprNode = buildExprNode {
+      _.setScalarFunction(PhysicalScalarFunctionNode.newBuilder()
+        .setName(fn.name())
+        .setFun(fn)
+        .addAllArgs(args.map(convertExpr).asJava)
+        .setReturnType(convertDataType(dataType))
+        .build())
     }
 
     sparkExpr match {
-      case Literal(value, dataType) => buildExprNode(_.setLiteral(convertValue(value, dataType)).build())
-      case AttributeReference(name, _, _, _) => buildExprNode(_.setColumn(PhysicalColumn.newBuilder().setName(name).build()))
+      case Literal(value, dataType) => buildExprNode {
+        _.setLiteral(convertValue(value, dataType))
+      }
+      case AttributeReference(name, _, _, _) => buildExprNode {
+        _.setColumn(PhysicalColumn.newBuilder().setName(name).build())
+      }
+
+      // cast
+      case Cast(child, dataType, _) => buildExprNode {
+        _.setCast(PhysicalCastNode.newBuilder()
+          .setExpr(convertExpr(child))
+          .setArrowType(convertDataType(dataType))
+          .build())
+      }
 
       // unary ops
-      case IsNotNull(child) => buildExprNode(_.setIsNotNullExpr(PhysicalIsNotNull.newBuilder().setExpr(convertExpr(child)).build()))
-      case Not(child) => buildExprNode(_.setNotExpr(PhysicalNot.newBuilder().setExpr(convertExpr(child)).build()))
+      case IsNotNull(child) => buildExprNode {
+        _.setIsNotNullExpr(PhysicalIsNotNull.newBuilder().setExpr(convertExpr(child)).build())
+      }
+      case Not(child) => buildExprNode {
+        _.setNotExpr(PhysicalNot.newBuilder().setExpr(convertExpr(child)).build())
+      }
 
       // binary ops
-      case EqualTo(lhs, rhs) => buildBinaryExprNode(convertExpr(lhs), convertExpr(rhs), "Eq")
-      case GreaterThan(lhs, rhs) => buildBinaryExprNode(convertExpr(lhs), convertExpr(rhs), "Gt")
-      case LessThan(lhs, rhs) => buildBinaryExprNode(convertExpr(lhs), convertExpr(rhs), "Lt")
-      case GreaterThanOrEqual(lhs, rhs) => buildBinaryExprNode(convertExpr(lhs), convertExpr(rhs), "GtEq")
-      case LessThanOrEqual(lhs, rhs) => buildBinaryExprNode(convertExpr(lhs), convertExpr(rhs), "LtEq")
-      case Add(lhs, rhs) => buildBinaryExprNode(convertExpr(lhs), convertExpr(rhs), "Plus")
-      case Subtract(lhs, rhs) => buildBinaryExprNode(convertExpr(lhs), convertExpr(rhs), "Minus")
-      case Multiply(lhs, rhs) => buildBinaryExprNode(convertExpr(lhs), convertExpr(rhs), "Multiply")
-      case Divide(lhs, rhs) => buildBinaryExprNode(convertExpr(lhs), convertExpr(rhs), "Divide")
-      case Like(lhs, rhs, '\\') => buildBinaryExprNode(convertExpr(lhs), convertExpr(rhs), "Like")
-      case And(lhs, rhs) => buildBinaryExprNode(convertExpr(lhs), convertExpr(rhs), "And")
-      case Or(lhs, rhs) => buildBinaryExprNode(convertExpr(lhs), convertExpr(rhs), "Or")
+      case EqualTo(lhs, rhs) => buildBinaryExprNode(lhs, rhs, "Eq")
+      case GreaterThan(lhs, rhs) => buildBinaryExprNode(lhs, rhs, "Gt")
+      case LessThan(lhs, rhs) => buildBinaryExprNode(lhs, rhs, "Lt")
+      case GreaterThanOrEqual(lhs, rhs) => buildBinaryExprNode(lhs, rhs, "GtEq")
+      case LessThanOrEqual(lhs, rhs) => buildBinaryExprNode(lhs, rhs, "LtEq")
+      case Add(lhs, rhs) => buildBinaryExprNode(lhs, rhs, "Plus")
+      case Subtract(lhs, rhs) => buildBinaryExprNode(lhs, rhs, "Minus")
+      case Multiply(lhs, rhs) => buildBinaryExprNode(lhs, rhs, "Multiply")
+      case Divide(lhs, rhs) => buildBinaryExprNode(lhs, rhs, "Divide")
+      case Like(lhs, rhs, '\\') => buildBinaryExprNode(lhs, rhs, "Like")
+      case And(lhs, rhs) => buildBinaryExprNode(lhs, rhs, "And")
+      case Or(lhs, rhs) => buildBinaryExprNode(lhs, rhs, "Or")
+
+      // builtin scalar functions
+      case e: Sqrt => buildScalarFunction(ScalarFunction.SQRT, e.children, e.dataType)
+      case e: Sin => buildScalarFunction(ScalarFunction.SIN, e.children, e.dataType)
+      case e: Cos => buildScalarFunction(ScalarFunction.COS, e.children, e.dataType)
+      case e: Tan => buildScalarFunction(ScalarFunction.TAN, e.children, e.dataType)
+      case e: Asin => buildScalarFunction(ScalarFunction.ASIN, e.children, e.dataType)
+      case e: Acos => buildScalarFunction(ScalarFunction.ACOS, e.children, e.dataType)
+      case e: Atan => buildScalarFunction(ScalarFunction.ATAN, e.children, e.dataType)
+      case e: Exp => buildScalarFunction(ScalarFunction.EXP, e.children, e.dataType)
+      // case Nothing => buildScalarFunction(ScalarFunction.LOG, Nil)
+      case e: Log2 => buildScalarFunction(ScalarFunction.LOG2, e.children, e.dataType)
+      case e: Log10 => buildScalarFunction(ScalarFunction.LOG10, e.children, e.dataType)
+      case e: Floor => buildScalarFunction(ScalarFunction.FLOOR, e.children, e.dataType)
+      case e: Ceil => buildScalarFunction(ScalarFunction.CEIL, e.children, e.dataType)
+      case Round(_1, Literal(0, _)) => buildScalarFunction(ScalarFunction.ROUND, Seq(_1), _1.dataType)
+      // case Nothing => buildScalarFunction(ScalarFunction.TRUNC, Nil)
+      case e: Abs => buildScalarFunction(ScalarFunction.ABS, e.children, e.dataType)
+      case e: Signum => buildScalarFunction(ScalarFunction.SIGNUM, e.children, e.dataType)
+      case e: OctetLength => buildScalarFunction(ScalarFunction.OCTETLENGTH, e.children, e.dataType)
+      case e: Concat => buildScalarFunction(ScalarFunction.CONCAT, e.children, e.dataType)
+      case e: Lower => buildScalarFunction(ScalarFunction.LOWER, e.children, e.dataType)
+      case e: Upper => buildScalarFunction(ScalarFunction.UPPER, e.children, e.dataType)
+      case e: StringTrim => buildScalarFunction(ScalarFunction.TRIM, e.srcStr +: e.trimStr.toSeq, e.dataType)
+      case e: StringTrimLeft => buildScalarFunction(ScalarFunction.LTRIM, e.srcStr +: e.trimStr.toSeq, e.dataType)
+      case e: StringTrimRight => buildScalarFunction(ScalarFunction.RTRIM, e.srcStr +: e.trimStr.toSeq, e.dataType)
+      // case Nothing => buildScalarFunction(ScalarFunction.TOTIMESTAMP, Nil)
+      // case Nothing => buildScalarFunction(ScalarFunction.ARRAY, Nil)
+      case e: NullIf => buildScalarFunction(ScalarFunction.NULLIF, e.children, e.dataType)
+      case e: DatePart => buildScalarFunction(ScalarFunction.DATEPART, e.children, e.dataType)
+      case e: TruncDate => buildScalarFunction(ScalarFunction.DATEPART, e.children, e.dataType)
+      case Md5(_1) => buildScalarFunction(ScalarFunction.MD5, Seq(_1), StringType)
+      case Sha2(_1, Literal(224, _)) => buildScalarFunction(ScalarFunction.SHA224, Seq(_1), StringType)
+      case Sha2(_1, Literal(  0, _)) => buildScalarFunction(ScalarFunction.SHA256, Seq(_1), StringType)
+      case Sha2(_1, Literal(256, _)) => buildScalarFunction(ScalarFunction.SHA256, Seq(_1), StringType)
+      case Sha2(_1, Literal(384, _)) => buildScalarFunction(ScalarFunction.SHA384, Seq(_1), StringType)
+      case Sha2(_1, Literal(512, _)) => buildScalarFunction(ScalarFunction.SHA512, Seq(_1), StringType)
+      case e: Log => buildScalarFunction(ScalarFunction.LN, e.children, e.dataType)
+      // case Nothing => buildScalarFunction(ScalarFunction.TOTIMESTAMPMILLIS, Nil)
 
       case unsupportedExpression =>
         throw new NotImplementedExpressionConversion(unsupportedExpression)
