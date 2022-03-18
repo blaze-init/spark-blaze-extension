c86dd1dbc1b907d63332bcdf43e355ea7fbd61e0
Yijie Shen <henry.yijieshen@gmail.com>
2022-03-18 19:53:31 +0800

    Use protoc from maven and remove all generated proto src

--- update-blaze-protobuf.sh
+++ update-blaze-protobuf.sh
@@ -8,7 +8,5 @@ proto_url="https://raw.githubusercontent.com/blaze-init/blaze-rs/$git_branch/pla
 
 proto_path="$PWD/src/main"
 proto_location="$proto_path/proto/plan.proto"
-java_out_dir="$proto_path/java"
 
 wget -O "$proto_location" "$proto_url"
-protoc -I "$proto_path" --java_out="$java_out_dir" "$proto_location"
