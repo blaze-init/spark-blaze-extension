0afe5b11f272b2aaeca4d3038b45b7ed01c72d81
Yijie Shen <henry.yijieshen@gmail.com>
2022-03-31 18:31:29 +0800

    Parquet range fix (#44)

--- src/main/scala/org/apache/spark/sql/blaze/execution/ArrowShuffleExchangeExec301.scala
+++ src/main/scala/org/apache/spark/sql/blaze/execution/ArrowShuffleExchangeExec301.scala
@@ -194,8 +194,7 @@ object ArrowShuffleExchangeExec301 {
       outputAttributes: Seq[Attribute],
       outputPartitioning: Partitioning,
       serializer: Serializer,
-      metrics: Map[String, SQLMetric]
-  ): ShuffleDependency[Int, InternalRow, InternalRow] = {
+      metrics: Map[String, SQLMetric]): ShuffleDependency[Int, InternalRow, InternalRow] = {
 
     val nativeInputRDD = rdd match {
       case rdd: NativeRDD => rdd
